b2d411278b48c49bdb2d7896a7198d743614ad1b
Krum Georgiev <krumgeorgiev@Krums-MacBook-Air.local>
2024-06-03 17:55:14 +0300

    Cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
-import postgres from 'postgres';
-
-export const sql = postgres(process.env.POSTGRES_URL, {
-  ssl: 'allow',
-});
-
 const nextConfig = {
   experimental: {
     ppr: true,
@@ -12,23 +6,6 @@ const nextConfig = {
     fetches: {
       fullUrl: true,
     },
-  },
-  transpilePackages: ['next-mdx-remote'],
-  async redirects() {
-    if (!process.env.POSTGRES_URL) {
-      return [];
-    }
-
-    let redirects = await sql`
-      SELECT source, destination, permanent
-      FROM redirects;
-    `;
-
-    return redirects.map(({ source, destination, permanent }) => ({
-      source,
-      destination,
-      permanent: !!permanent,
-    }));
   },
   headers() {
     return [
