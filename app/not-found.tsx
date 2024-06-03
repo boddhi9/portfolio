c38d79c5c7bb384ce2396fd213452b9c343a2ac5
Krum Georgiev <krumgeorgiev@Krums-MacBook-Air.local>
2024-06-03 17:49:31 +0300

    Next lint added

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">
-        Oh no! This page doesn't exist.
+        Oh no! This page doesn&apos;t exist.
       </h1>
     </section>
   );
