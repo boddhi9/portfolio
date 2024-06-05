e8c0a47ec2b06fd783ec72d03287c07f93d71f02
Krum Georgiev <krumgeorgiev@Krums-MacBook-Air.local>
2024-06-06 01:32:04 +0300

    Cleanup

--- app/work/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <section>
-      <h1 className="font-chivo text-2xl mb-8 tracking-tighter">my work</h1>
-      <p className="prose prose-neutral dark:prose-invert">coming soon</p>
-    </section>
-  )
-}
