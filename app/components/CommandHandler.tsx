935b6af220b09e67f5ecd991e2dfe16c5f2eb095
Krum Georgiev <krumgeorgiev@Krums-MacBook-Air.local>
2024-06-05 13:52:09 +0300

    Added CV

---
+++ app/components/CommandHandler.tsx
@@ -0,0 +1,140 @@
+import React, { useState } from 'react'
+
+type Props = {
+  command: string
+  commandArguments: string
+  setShowWelcomeMessage: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const CommandHandler = ({
+  command,
+  commandArguments,
+  setShowWelcomeMessage,
+}: Props) => {
+  setShowWelcomeMessage(false)
+  const [downloadTriggered, setDownloadTriggered] = useState(false)
+
+  const downloadCV = () => {
+    const link = document.createElement('a')
+    link.href = '/Profile.pdf'
+    link.download = 'Profile.pdf'
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
+
+  const commands = {
+    help: () => (
+      <span>
+        <strong>clear</strong> - clears the console <br />
+        <strong>about</strong> - info about me <br />
+        <strong>experience (or just exp)</strong> - my experience <br />
+        <strong>get cv</strong> - download my CV <br />
+      </span>
+    ),
+    about: () => (
+      <pre>
+        {JSON.stringify(
+          {
+            name: 'Krum Georgiev',
+            currentPosition: 'Staff Software Engineer at Vercel',
+            languages: 'JavaScript, TypeScript, Erlang, WebAssembly, Swift',
+            frameworks:
+              'React, Next.js, Node.js, Deno, Bun, Remix, Svelte, NestJS',
+            tools: 'GitHub, GitLab, Bitbucket, Docker, CircleCI, AWS, Figma',
+            softSkills:
+              'Agile Method, Teamwork, Communication, Problem-solving',
+            favoriteIDEs: 'VS Code, Zed',
+            city: 'Sofia, Bulgaria',
+          },
+          null,
+          2,
+        )}
+      </pre>
+    ),
+    exp: () => (
+      <span>
+        <p>
+          Ever since I was a kid, software development has been my passion. From
+          BASIC on an Atari 800 to mastering various languages and frameworks,
+          my journey led me to a BSc in Computer Science at New Bulgarian
+          University.
+        </p>
+        <br />
+        <p>
+          With over 25 years in the industry, I have contributed to major
+          projects at Meta, Bosch, BBC, CNN, Adobe, Google, and more.
+        </p>
+        <br />
+        <p>
+          My expertise lies in frontend technologies, focusing on user
+          experience and innovative solutions. I thrive on tackling challenges
+          and staying at the forefront of tech advancements.
+        </p>
+      </span>
+    ),
+    ls: () => {
+      if (!commandArguments) return 'app'
+      if (commandArguments === '-a') {
+        return (
+          <div>
+            <div>.bun</div>
+            <div>.node-gyp</div>
+            <div>.npm</div>
+            <div>.yarn</div>
+          </div>
+        )
+      } else if (commandArguments === '-lah') {
+        return (
+          <div>
+            <div>drwxr-xr-x 4 krumgeorgiev wheel 384B Jun 4 09:52 .</div>
+            <div>-rw-r--r-- 3 krumgeorgiev staff 18K May 23 23:18 .bun</div>
+            <div>
+              drwxr-xr-x 4 krumgeorgiev staff 569B Jun 2 16:44 .node-gyp
+            </div>
+            <div>drwxr-xr-x 5 krumgeorgiev staff 114K Jun 8 13:23 .npm</div>
+            <div>drwxr-xr-x 7 krumgeorgiev staff 56K Jun 12 22:42 .yarn</div>
+          </div>
+        )
+      } else {
+        return `ls: illegal option -- ${commandArguments}. Nice try, maybe you should stick to '-a' or '-lah'.`
+      }
+    },
+    cd: () => {
+      const allowedDirs = ['.bun', '.node-gyp', '.npm', '.yarn', 'app']
+      if (!commandArguments || commandArguments === '..') {
+        return "Oops! You can't escape that easily. Staying put in this directory!"
+      }
+      if (allowedDirs.includes(commandArguments)) {
+        return `zsh: cd: ${commandArguments}: Permission denied`
+      }
+      return `cd: no such file or directory: ${commandArguments}`
+    },
+    get: () => {
+      if (commandArguments === 'cv') {
+        if (!downloadTriggered) {
+          setDownloadTriggered(true)
+          downloadCV()
+          return 'Downloading CV...'
+        }
+        return ''
+      }
+      return `get: invalid argument: ${commandArguments}`
+    },
+    sudo: () => {
+      if (!commandArguments) {
+        return 'usage: sudo -h | -K | -k | -V'
+      }
+      if (commandArguments === 'rm -rf') {
+        return 'Nope! Not falling for that one!'
+      }
+      return `sudo: a password is required, but sorry, I forgot it`
+    },
+  }
+
+  return commands[command]
+    ? commands[command]()
+    : `zsh: command not found: ${command}`
+}
+
+export default CommandHandler
--- app/page.tsx
+++ app/page.tsx
@@ -4,179 +4,92 @@ import { useEffect, useState } from 'react'
 
 import { ReactTerminal, TerminalContextProvider } from 'react-terminal'
 
+import CommandHandler from './components/CommandHandler'
+
 export default function Page() {
   const [isLoading, setIsLoading] = useState(true)
-  const [inputEnabled, setInputEnabled] = useState(true)
   const [showWelcomeMessage, setShowWelcomeMessage] = useState(true)
-  const canRender = !isLoading
 
   useEffect(() => {
     setIsLoading(false)
-    setInputEnabled(true)
   }, [])
 
-  const handleCommand = (command: string, commandArguments: string) => {
-    if (showWelcomeMessage) {
-      setShowWelcomeMessage(false)
-    }
+  const welcomeMessage = showWelcomeMessage && (
+    <section className="container mx-auto">
+      <header>
+        <h1 className="text-md font-bold">Krum Georgiev</h1>
+        <p className="text-md">
+          Tech enthusiast | Staff Engineer at Vercel | TC39 member | Stuck in
+          Vim
+        </p>
+      </header>
 
-    switch (command) {
-      case 'help':
-      case '?':
-        return (
-          <span>
-            <strong>clear</strong> - clears the console <br />
-            <strong>about</strong> - info about me <br />
-            <strong>experience (or just exp)</strong> - my experience <br />
-            <strong>get cv</strong> - download my CV <br />
-          </span>
-        )
-      case 'about': {
-        const aboutInfo = {
-          name: 'Krum Georgiev',
-          currentPosition: 'Staff Software Engineer at Vercel',
-          languages: 'JavaScript, TypeScript, Erlang, WebAssembly, Swift',
-          frameworks:
-            'React, Next.js, Node.js, Deno, Bun, Remix, Svelte, NestJS',
-          tools: 'GitHub, GitLab, Bitbucket, Docker, CircleCI, AWS, Figma',
-          softSkills: 'Agile Method, Teamwork, Communication, Problem-solving',
-          favoriteIDEs: 'VS Code, Zed',
-          city: 'Sofia, Bulgaria',
-        }
-        return <pre>{JSON.stringify(aboutInfo, null, 2)}</pre>
-      }
-      case 'exp':
-      case 'experience':
-        return (
-          <span>
-            <p>
-              Ever since I was a kid, software development has been my passion.
-              From BASIC on an Atari 800 to mastering various languages and
-              frameworks, my journey led me to a BSc in Computer Science at New
-              Bulgarian University.
-            </p>
-            <br />
-            <p>
-              With over 25 years in the industry, I have contributed to major
-              projects at Meta, Bosch, BBC, CNN, Adobe, Google, and more.
-            </p>
-            <br />
-            <p>
-              My expertise lies in frontend technologies, focusing on user
-              experience and innovative solutions. I thrive on tackling
-              challenges and staying at the forefront of tech advancements.
-            </p>
-          </span>
-        )
-      case 'ls':
-        if (!commandArguments) return ''
-        if (commandArguments === '-a') {
-          return (
-            <div>
-              <div>.bun</div>
-              <div>.node-gyp</div>
-              <div>.npm</div>
-              <div>.yarn</div>
-            </div>
-          )
-        } else if (commandArguments === '-lah') {
-          return (
-            <div>
-              <div>drwxr-xr-x 4 krumgeorgiev wheel 384B Jun 4 09:52 .</div>
-              <div>-rw-r--r-- 3 krumgeorgiev staff 18K May 23 23:18 .bun</div>
-              <div>
-                drwxr-xr-x 4 krumgeorgiev staff 569B Jun 2 16:44 .node-gyp
-              </div>
-              <div>drwxr-xr-x 5 krumgeorgiev staff 114K Jun 8 13:23 .npm</div>
-              <div>drwxr-xr-x 7 krumgeorgiev staff 56K Jun 12 22:42 .yarn</div>
-            </div>
-          )
-        } else {
-          return `ls: illegal option -- ${commandArguments}. Nice try, maybe you should stick to '-a' or '-lah'.`
-        }
-      case 'cd': {
-        const allowedDirs = ['.bun', '.node-gyp', '.npm', '.yarn']
-        if (
-          !commandArguments ||
-          commandArguments === '..' ||
-          commandArguments === ''
-        ) {
-          return "Oops! You can't escape that easily. Staying put in this directory!"
-        }
-        if (allowedDirs.includes(commandArguments)) {
-          return `zsh: cd: ${commandArguments}: Permission denied`
-        }
-        return `cd: no such file or directory: ${commandArguments}`
-      }
-      case 'get': {
-        if (commandArguments && commandArguments === 'cv') {
-          return 'Coming soon!'
-        }
-        if (!commandArguments) {
-          return 'get: missing argument, try "get cv"'
-        }
-        return `get: invalid argument: ${commandArguments}`
-      }
-      case 'sudo':
-        if (!commandArguments || commandArguments === '') {
-          return 'usage: sudo -h | -K | -k | -V'
-        }
-        if (commandArguments === 'rm -rf') {
-          return 'Nope! Not falling for that one!'
-        }
-        return `sudo: a password is required, but sorry, I forgot it`
-      default:
-        return `zsh: command not found: ${command}`
-    }
-  }
+      <section>
+        <h2 className="text-md font-semibold">Previously:</h2>
+        <ul className="list-disc pl-5">
+          <li>Senior Software Engineer at Patient21 (Jan 2021 - May 2024 )</li>
+          <li>Principal Engineer at Facebook (Jan 2020 - Jan 2021 )</li>
+          <li>Senior Front End Engineer at Bosch (Apr 2018 - Jan 2020)</li>
+          <li>Development Lead at Adobe (Sep 2016 - Mar 2018)</li>
+          <li>Senior Software Engineer at Ontotext (Aug 2007 - Sep 2016)</li>
+        </ul>
+      </section>
 
-  const welcomeMessage = showWelcomeMessage ? (
-    <>
-      <div className="container">
-        <div className="header">
-          <h2>Krum Georgiev</h2>
-          <p>Tech enthusiast | Staff Engineer at Vercel | TC39 member</p>
-        </div>
-        <h2>Previously:</h2>
-        <div className="header">
-          <p>Principal Engineer at Facebook</p>
-          <p>Lead Engineer at Adobe</p>
-          <p>Senior Front End Engineer at Bosch</p>
-        </div>
-        <h2>More:</h2>
-        <div className="header">
-          <p>
-            <a href="https://linkedin.com/in/krumgeorgiev">LinkedIn</a>
-          </p>
-          <p>
-            <a>Download CV (soon)</a>
-          </p>
-        </div>
-        <br />
-        <h2>
+      <section>
+        <h2 className="text-md font-semibold">More:</h2>
+        <ul className="list-disc pl-5">
+          <li>
+            <a
+              href="https://linkedin.com/in/krumgeorgiev"
+              className="text-blue-500 underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="/Profile.pdf"
+              className="text-gray-500 underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Download CV
+            </a>
+          </li>
+        </ul>
+      </section>
+
+      <footer>
+        <h2 className="text-md font-semibold">
           Type &quot;help&quot; or &quot;?&quot; for all available commands. Try
           &quot;about&quot; to learn more about me and &quot;exp&quot; to see my
           experience.
         </h2>
-      </div>
-    </>
-  ) : null
+      </footer>
+    </section>
+  )
 
   return (
     <TerminalContextProvider>
       <div className="terminal">
-        {canRender && (
+        {!isLoading && (
           <ReactTerminal
             theme="material-ocean"
-            enableInput={inputEnabled}
+            enableInput
             prompt="Krum:~/dev$"
             welcomeMessage={welcomeMessage}
-            defaultHandler={(command: string, commandArguments: string) =>
-              handleCommand(command, commandArguments)
+            defaultHandler={(command: string, commandArguments: string) => (
+              <CommandHandler
+                command={command}
+                commandArguments={commandArguments}
+                setShowWelcomeMessage={setShowWelcomeMessage}
+              />
+            )}
+            errorMessage={(command: string) =>
+              `zsh: command not found: ${command}`
             }
-            errorMessage={(command: string) => {
-              return `zsh: command not found: ${command}`
-            }}
           />
         )}
       </div>
