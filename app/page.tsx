ecce40f7f4ef6e761e5d224c4a5a653669660573
Krum Georgiev <krumgeorgiev@Krums-MacBook-Air.local>
2024-06-05 15:24:18 +0300

    Responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { ReactTerminal, TerminalContextProvider } from 'react-terminal'
+
+import useMediaQuery from '@custom-react-hooks/use-media-query'
 
 import CommandHandler from './components/CommandHandler'
 
 export default function Page() {
   const [isLoading, setIsLoading] = useState(true)
-  const [showWelcomeMessage, setShowWelcomeMessage] = useState(true)
+  const [, setShowWelcomeMessage] = useState(true)
+  const isMobile = useMediaQuery('(max-width: 768px)')
 
   useEffect(() => {
     setIsLoading(false)
   }, [])
 
-  const welcomeMessage = showWelcomeMessage && (
-    <section className="container mx-auto">
+  const welcomeMessage = (
+    <section className="container mx-auto px-4">
       <header>
         <h1 className="text-md font-bold">Krum Georgiev</h1>
         <p className="text-md">
@@ -23,18 +26,16 @@ export default function Page() {
           Vim
         </p>
       </header>
-
       <section>
         <h2 className="text-md font-semibold">Previously:</h2>
         <ul className="list-disc pl-5">
-          <li>Senior Software Engineer at Patient21 (Jan 2021 - May 2024 )</li>
-          <li>Principal Engineer at Facebook (Jan 2020 - Jan 2021 )</li>
+          <li>Senior Software Engineer at Patient21 (Jan 2021 - May 2024)</li>
+          <li>Principal Engineer at Facebook (Jan 2020 - Jan 2021)</li>
           <li>Senior Front End Engineer at Bosch (Apr 2018 - Jan 2020)</li>
           <li>Development Lead at Adobe (Sep 2016 - Mar 2018)</li>
           <li>Senior Software Engineer at Ontotext (Aug 2007 - Sep 2016)</li>
         </ul>
       </section>
-
       <section>
         <h2 className="text-md font-semibold">More:</h2>
         <ul className="list-disc pl-5">
@@ -60,37 +61,44 @@ export default function Page() {
           </li>
         </ul>
       </section>
-
-      <footer>
-        <h2 className="text-md font-semibold">
-          Type &quot;help&quot; or &quot;?&quot; for all available commands. Try
-          &quot;about&quot; to learn more about me and &quot;exp&quot; to see my
-          experience.
-        </h2>
-      </footer>
+      {!isMobile && (
+        <footer>
+          <h2 className="text-md font-semibold">
+            Type &quot;help&quot; or &quot;?&quot; for all available commands.
+            Try &quot;about&quot; to learn more about me and &quot;exp&quot; to
+            see my experience.
+          </h2>
+        </footer>
+      )}
     </section>
   )
 
   return (
-    <TerminalContextProvider>
-      {!isLoading && (
-        <ReactTerminal
-          theme="material-ocean"
-          enableInput
-          prompt="Krum:~/dev$"
-          welcomeMessage={welcomeMessage}
-          defaultHandler={(command: string, commandArguments: string) => (
-            <CommandHandler
-              command={command}
-              commandArguments={commandArguments}
-              setShowWelcomeMessage={setShowWelcomeMessage}
+    <>
+      {!isMobile ? (
+        <TerminalContextProvider>
+          {!isLoading && (
+            <ReactTerminal
+              theme="material-ocean"
+              enableInput
+              prompt="Krum:~/dev$"
+              welcomeMessage={welcomeMessage}
+              defaultHandler={(command: string, commandArguments: string) => (
+                <CommandHandler
+                  command={command}
+                  commandArguments={commandArguments}
+                  setShowWelcomeMessage={setShowWelcomeMessage}
+                />
+              )}
+              errorMessage={(command: string) =>
+                `zsh: command not found: ${command}`
+              }
             />
           )}
-          errorMessage={(command: string) =>
-            `zsh: command not found: ${command}`
-          }
-        />
+        </TerminalContextProvider>
+      ) : (
+        welcomeMessage
       )}
-    </TerminalContextProvider>
+    </>
   )
 }
