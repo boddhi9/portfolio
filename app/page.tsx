e00293f744ba97076cfd8894b91f4e0fddc3e446
Krum Georgiev <krumgeorgiev@Krums-MacBook-Air.local>
2024-06-05 12:54:27 +0300

    Minor update

--- app/page.tsx
+++ app/page.tsx
@@ -6,104 +6,175 @@ import { ReactTerminal, TerminalContextProvider } from 'react-terminal'
 
 export default function Page() {
   const [isLoading, setIsLoading] = useState(true)
+  const [inputEnabled, setInputEnabled] = useState(true)
+  const [showWelcomeMessage, setShowWelcomeMessage] = useState(true)
+  const canRender = !isLoading
 
   useEffect(() => {
     setIsLoading(false)
+    setInputEnabled(true)
   }, [])
 
-  const commands = {
-    help: (
-      <span>
-        <strong>clear</strong> - clears the console <br />
-        <strong>about</strong> - some info about me <br />
-        <strong>exp</strong> - experience <br />
-      </span>
-    ),
-    about: (
-      <span>
-        <strong>Tech enthusiast</strong>
-        <br />
-        <strong>Staff Engineer at Vercel</strong>
-        <br />
-        <strong>TC39 member</strong>
-        <br />
-        <strong>Skydiver</strong>
-        <br />
-      </span>
-    ),
-    exp: (
-      <span>
-        <strong>Tech enthusiast</strong>
-        <br />
-        <strong>Staff Engineer at Vercel</strong>
-        <br />
-        <strong>TC39 member</strong>
-        <br />
-        <strong>Skydiver</strong>
-        <br />
-      </span>
-    ),
-    ls: (
-      <div>
-        <div>bin</div>
-        <div>dev</div>
-        <div>nvim-macos</div>
-        <div>npm</div>
-      </div>
-    ),
-    cd: (dir) => {
-      const allowedDirs = ['bin', 'dev', 'nvim-macos', 'npm']
-      if (!dir || dir === '.' || dir === '..' || dir === '') {
-        return ''
+  const handleCommand = (command: string, commandArguments: string) => {
+    if (showWelcomeMessage) {
+      setShowWelcomeMessage(false)
+    }
+
+    switch (command) {
+      case 'help':
+      case '?':
+        return (
+          <span>
+            <strong>clear</strong> - clears the console <br />
+            <strong>about</strong> - info about me <br />
+            <strong>experience (or just exp)</strong> - my experience <br />
+            <strong>get cv</strong> - download my CV <br />
+          </span>
+        )
+      case 'about': {
+        const aboutInfo = {
+          name: 'Krum Georgiev',
+          currentPosition: 'Staff Software Engineer at Vercel',
+          languages: 'JavaScript, TypeScript, Erlang, WebAssembly, Swift',
+          frameworks:
+            'React, Next.js, Node.js, Deno, Bun, Remix, Svelte, NestJS',
+          tools: 'GitHub, GitLab, Bitbucket, Docker, CircleCI, AWS, Figma',
+          softSkills: 'Agile Method, Teamwork, Communication, Problem-solving',
+          favoriteIDEs: 'VS Code, Zed',
+          city: 'Sofia, Bulgaria',
+        }
+        return <pre>{JSON.stringify(aboutInfo, null, 2)}</pre>
       }
-      if (allowedDirs.includes(dir)) {
-        return `zsh: cd: ${dir}: Permission denied`
+      case 'exp':
+      case 'experience':
+        return (
+          <span>
+            <p>
+              Ever since I was a kid, software development has been my passion.
+              From BASIC on an Atari 800 to mastering various languages and
+              frameworks, my journey led me to a BSc in Computer Science at New
+              Bulgarian University.
+            </p>
+            <br />
+            <p>
+              With over 25 years in the industry, I have contributed to major
+              projects at Meta, Bosch, BBC, CNN, Adobe, Google, and more.
+            </p>
+            <br />
+            <p>
+              My expertise lies in frontend technologies, focusing on user
+              experience and innovative solutions. I thrive on tackling
+              challenges and staying at the forefront of tech advancements.
+            </p>
+          </span>
+        )
+      case 'ls':
+        if (!commandArguments) return ''
+        if (commandArguments === '-a') {
+          return (
+            <div>
+              <div>.bun</div>
+              <div>.node-gyp</div>
+              <div>.npm</div>
+              <div>.yarn</div>
+            </div>
+          )
+        } else if (commandArguments === '-lah') {
+          return (
+            <div>
+              <div>drwxr-xr-x 4 krumgeorgiev wheel 384B Jun 4 09:52 .</div>
+              <div>-rw-r--r-- 3 krumgeorgiev staff 18K May 23 23:18 .bun</div>
+              <div>
+                drwxr-xr-x 4 krumgeorgiev staff 569B Jun 2 16:44 .node-gyp
+              </div>
+              <div>drwxr-xr-x 5 krumgeorgiev staff 114K Jun 8 13:23 .npm</div>
+              <div>drwxr-xr-x 7 krumgeorgiev staff 56K Jun 12 22:42 .yarn</div>
+            </div>
+          )
+        } else {
+          return `ls: illegal option -- ${commandArguments}. Nice try, maybe you should stick to '-a' or '-lah'.`
+        }
+      case 'cd': {
+        const allowedDirs = ['.bun', '.node-gyp', '.npm', '.yarn']
+        if (
+          !commandArguments ||
+          commandArguments === '..' ||
+          commandArguments === ''
+        ) {
+          return "Oops! You can't escape that easily. Staying put in this directory!"
+        }
+        if (allowedDirs.includes(commandArguments)) {
+          return `zsh: cd: ${commandArguments}: Permission denied`
+        }
+        return `cd: no such file or directory: ${commandArguments}`
       }
-      return `cd: no such file or directory: ${dir}`
-    },
+      case 'get': {
+        if (commandArguments && commandArguments === 'cv') {
+          return 'Coming soon!'
+        }
+        if (!commandArguments) {
+          return 'get: missing argument, try "get cv"'
+        }
+        return `get: invalid argument: ${commandArguments}`
+      }
+      case 'sudo':
+        if (!commandArguments || commandArguments === '') {
+          return 'usage: sudo -h | -K | -k | -V'
+        }
+        if (commandArguments === 'rm -rf') {
+          return 'Nope! Not falling for that one!'
+        }
+        return `sudo: a password is required, but sorry, I forgot it`
+      default:
+        return `zsh: command not found: ${command}`
+    }
   }
-  const welcomeMessage = (
+
+  const welcomeMessage = showWelcomeMessage ? (
     <>
       <div className="container">
         <div className="header">
-          <p>Tech enthusiast</p>
-          <p>Staff Engineer at Vercel</p>
-          <p>TC39 member</p>
-          <p>Skydiver</p>
-          <p>Stuck in Vim</p>
+          <h2>Krum Georgiev</h2>
+          <p>Tech enthusiast | Staff Engineer at Vercel | TC39 member</p>
         </div>
         <h2>Previously:</h2>
-        <ul>
-          <li>Staff Engineer at Facebook</li>
-          <li>Lead Engineer at Adobe</li>
-          <li>Senior Front End Engineer at Bosch</li>
-        </ul>
-        <h2>Where I hide:</h2>
-        <ul>
-          <li>
+        <div className="header">
+          <p>Principal Engineer at Facebook</p>
+          <p>Lead Engineer at Adobe</p>
+          <p>Senior Front End Engineer at Bosch</p>
+        </div>
+        <h2>More:</h2>
+        <div className="header">
+          <p>
             <a href="https://linkedin.com/in/krumgeorgiev">LinkedIn</a>
-          </li>
-          <li>
-            <a href="https://www.google.com/maps/@42.6798277,23.3590247,12.59z?entry=ttu">
-              Sofia
-            </a>
-          </li>
-        </ul>
-        <h2>Type &quot;help&quot; for all available commands.</h2>
+          </p>
+          <p>
+            <a>Download CV (soon)</a>
+          </p>
+        </div>
+        <br />
+        <h2>
+          Type &quot;help&quot; or &quot;?&quot; for all available commands. Try
+          &quot;about&quot; to learn more about me and &quot;exp&quot; to see my
+          experience.
+        </h2>
       </div>
     </>
-  )
+  ) : null
+
   return (
     <TerminalContextProvider>
       <div className="terminal">
-        {!isLoading && (
+        {canRender && (
           <ReactTerminal
             theme="material-ocean"
-            showControlBar={false}
-            commands={commands}
-            prompt="🪂 boddhi: ~"
+            enableInput={inputEnabled}
+            prompt="Krum:~/dev$"
             welcomeMessage={welcomeMessage}
-            errorMessage={(command) => {
+            defaultHandler={(command: string, commandArguments: string) =>
+              handleCommand(command, commandArguments)
+            }
+            errorMessage={(command: string) => {
               return `zsh: command not found: ${command}`
             }}
           />
